0b767223b9e46cd90c552cd10c4779a792fa9ca1
chengjiwang <chengji3728@gmail.com>
2018-06-06 16:54:16 +0800

    modify layout and add edit block

---
+++ source/javascripts/all.js
@@ -0,0 +1,14 @@
+console.log(5);
+var app = new Vue({
+  el: '#app',
+  data : {
+    newTodo: '',
+    todos: [
+      {
+        id: '35',
+        title: 'alvin',
+        completed: false
+      }
+    ],
+  }
+});
