7fe5f2f6df3171e1bc0f7da2dfe3f5cb09a085b8
chengjiwang <chengji3728@gmail.com>
2018-06-06 18:29:17 +0800

    add basic todo and progress switch

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -3,12 +3,42 @@ var app = new Vue({
   el: '#app',
   data : {
     newTodo: '',
-    todos: [
-      {
-        id: '35',
-        title: 'alvin',
+    editmode: false,
+    todos: [],
+    cacheTodo: {},
+    cacheTitle: '',
+    visibility: 'all'
+  },
+  methods: {
+    addTodo: function(){
+      var value = this.newTodo.trim();
+      var timestamp = Math.floor(Date.now());
+      if(!value){return;}
+      this.todos.push({
+        id: timestamp,
+        title: value,
         completed: false
+      });
+      this.newTodo = '';
+      this.editmode = false;
+    }
+  },
+  computed : {
+    filterTodo : function(){
+      if(this.visibility == 'all'){
+        return this.todos;
+      } else if(this.visibility == 'progress'){
+        return this.todos.filter(function(item,index,array){
+          console.log(item);
+          return !item.completed;
+        })
+      } else if(this.visibility == 'completed'){
+        return this.todos.filter(function(item){
+          return item.completed;
+        })
       }
-    ],
+      return [];
+    }
   }
+  
 });
