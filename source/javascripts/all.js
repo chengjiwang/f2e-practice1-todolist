4bd63772ce87c4ec34d92614639e2e7d6da7ec42
chengjiwang <chengji3728@gmail.com>
2018-06-07 22:56:33 +0800

    add remaining task and update todos

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,26 +1,58 @@
-console.log(5);
 var app = new Vue({
   el: '#app',
   data : {
     newTodo: '',
+    deadline: '',
+    comment: '',
     editmode: false,
     todos: [],
     cacheTodo: {},
     cacheTitle: '',
+    cacheKey: '',
     visibility: 'all'
   },
   methods: {
+    setFormDefault: function(){
+      this.newTodo = '';
+      this.deadline = '';
+      this.comment = '';
+      this.editmode = false;
+    },
     addTodo: function(){
       var value = this.newTodo.trim();
-      var timestamp = Math.floor(Date.now());
       if(!value){return;}
+      var timestamp = Math.floor(Date.now());
+      var endDate = this.deadline;
+      var message = this.comment;
       this.todos.push({
         id: timestamp,
         title: value,
-        completed: false
+        completed: false,
+        deadline: endDate,
+        comment: message
       });
-      this.newTodo = '';
-      this.editmode = false;
+      this.setFormDefault();
+    },
+    cancelTodo: function(){
+      this.setFormDefault();
+    },
+    editTodo: function(item,key){
+      this.editmode = true;
+      console.log(item);
+      console.log(key);
+      this.cacheKey = key;
+      this.newTodo = item.title;
+      this.deadline = item.deadline;
+      this.comment = item.comment;
+    },
+    updateTodo : function(){
+      console.log(typeof this.cacheKey);
+      console.log(this.todos[this.cacheKey]);
+      var item = this.todos[this.cacheKey];
+      item.title = this.newTodo;
+      item.deadline = this.deadline;
+      item.comment = this.comment;
+      this.setFormDefault();
     }
   },
   computed : {
@@ -38,7 +70,11 @@ var app = new Vue({
         })
       }
       return [];
+    },
+    remaining: function(){
+      return (this.todos.filter(function(item){
+        return !item.completed;
+      })).length;
     }
   }
-  
 });
