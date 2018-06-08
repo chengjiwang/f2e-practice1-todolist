64e5e132d5e6641aeb59676a2cd7e3f16fb42f24
chengjiwang <chengji3728@gmail.com>
2018-06-08 20:55:53 +0800

    新增star標示為重要功能，切換顯示Add Task和Save按鈕

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -4,19 +4,37 @@ var app = new Vue({
     newTodo: '',
     deadline: '',
     comment: '',
-    editmode: false,
-    todos: [],
-    cacheTodo: {},
-    cacheTitle: '',
+    isNewTodo: false,
+    editMode: false,
+    todos: [
+      {
+        'id': 01,
+        'title': '運動',
+        'completed': false,
+        'deadline': '2018-06-10',
+        'comment': '至少30分',
+        'stared': true
+      },
+      {
+        'id': 02,
+        'title': '打掃房間',
+        'completed': false,
+        'deadline': '2018-06-12',
+        'comment': '',
+        'stared': false
+      }
+    ],
     cacheKey: '',
-    visibility: 'all'
+    visibility: 'all',
+    isImportantStar: false
   },
   methods: {
     setFormDefault: function(){
       this.newTodo = '';
       this.deadline = '';
       this.comment = '';
-      this.editmode = false;
+      this.isNewTodo = false;
+      this.editMode = false;
     },
     addTodo: function(){
       var value = this.newTodo.trim();
@@ -29,30 +47,34 @@ var app = new Vue({
         title: value,
         completed: false,
         deadline: endDate,
-        comment: message
+        comment: message,
+        stared: false
       });
       this.setFormDefault();
     },
     cancelTodo: function(){
       this.setFormDefault();
     },
+    deleteTodo: function(key){
+      this.todos.splice(key,1);
+    },
     editTodo: function(item,key){
-      this.editmode = true;
-      console.log(item);
-      console.log(key);
+      this.isNewTodo = true;
+      this.editMode = true;
       this.cacheKey = key;
       this.newTodo = item.title;
       this.deadline = item.deadline;
       this.comment = item.comment;
     },
     updateTodo : function(){
-      console.log(typeof this.cacheKey);
-      console.log(this.todos[this.cacheKey]);
       var item = this.todos[this.cacheKey];
       item.title = this.newTodo;
       item.deadline = this.deadline;
       item.comment = this.comment;
       this.setFormDefault();
+    },
+    updateStar: function(item,status){
+      item.stared = status;
     }
   },
   computed : {
@@ -61,7 +83,6 @@ var app = new Vue({
         return this.todos;
       } else if(this.visibility == 'progress'){
         return this.todos.filter(function(item,index,array){
-          console.log(item);
           return !item.completed;
         })
       } else if(this.visibility == 'completed'){
